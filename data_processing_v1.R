436240b33bf84f6ad150a16a608377a1794685c1
iamdudumon <kksy4511@naver.com>
2023-11-24 17:50:12 +0900

    feat: 온습도, 이슬점, 전력량 정규화 -> 활동량을 의미하는 activity_index 컬럼 생성 + activity_index 라인 그래프

--- data_processing_v1.R
+++ data_processing_v1.R
@@ -29,7 +29,6 @@ freq(df$door3, plot=T)
 boxplot(df$plug)
 
 
-
 ### 스케일링
 sensor_cols <- c("temperature", "humidity", "dew_point", "plug")
 
@@ -40,9 +39,29 @@ df[sensor_cols] <- lapply(df[sensor_cols], function(x) {
 summary(df)
 
 df_10 <- df %>% filter(date >= as.POSIXct("2023-10-01"))
-summary(df_10)
 
 df_10$activity_index_morning = ifelse(df_10$time_of_day == "Morning", rowSums(df_10[, c("door1", "door2", "door3", "motion1", "motion2", "plug")], na.rm = TRUE), NA)
 df_10$activity_index_evening = ifelse(df_10$time_of_day == "Evening", rowSums(df_10[, c("door1", "door2", "door3", "motion1", "motion2", "plug")], na.rm = TRUE), NA)
 df_10$activity_index_afternoon = ifelse(df_10$time_of_day == "Afternoon", rowSums(df_10[, c("door1", "door2", "door3", "motion1", "motion2", "plug")], na.rm = TRUE), NA)
 df_10$activity_index_Night = ifelse(df_10$time_of_day == "Night", rowSums(df_10[, c("door1", "door2", "door3", "motion1", "motion2", "plug")], na.rm = TRUE), NA)
+
+# 정규화
+df_10 <- df %>% filter(date >= as.POSIXct("2023-10-01"))
+
+library(scales)
+library(dplyr)
+df_10 <- df_10 %>%
+  mutate(
+    temperature = rescale(temperature, to = c(0, 1)),
+    humidity = rescale(humidity, to = c(0, 1)),
+    dew_point = rescale(dew_point, to = c(0, 1)),
+    plug = rescale(plug, to = c(0, 10))
+  )
+
+weights = c(door1 = 1, door2 = 1, door3 = 1, motion1 = 1, motion2 = 1, plug = 1)
+df_10$activity_index = rowSums(sweep(df_10[, c("door1", "door2", "door3", "motion1", "motion2", "plug")], 2, weights, "*"), na.rm = TRUE)
+
+
+library(ggplot2)
+ggplot() +
+  geom_line(data=df_10, aes(x=date, y=activity_index))
